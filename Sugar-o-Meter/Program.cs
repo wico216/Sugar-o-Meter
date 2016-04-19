6b9ff5aab4bd58939dbd3ab1754b24f17421d28e
Juan <Juan@JUAN-PC>
2016-04-19 10:43:27 -0400

    Add project files.

---
+++ Sugar-o-Meter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sugar_o_Meter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Readings read = new Readings();
+            float Sugarlvl = 0;
+            
+            Console.WriteLine("Please enter your current blood sugar: \b");
+            Sugarlvl = float.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Your sugar level is: " + Sugarlvl);
+           
+
+        }
+    }
+}
---
+++ Sugar-o-Meter/Readings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sugar_o_Meter
+{
+    class Readings
+    {
+        public Readings()
+        {
+            readings = new List<float>();
+        }
+
+        public void AddReading(float sugar)
+        {
+            readings.Add(sugar);
+        }
+
+        List<float> readings; 
+    }
+}
